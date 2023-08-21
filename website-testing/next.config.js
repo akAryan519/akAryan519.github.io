4adb61b3b6876a02f582f70a9291ca24a66e1ee9
Aryan <akaryan519@gmail.com>
2023-08-21 18:23:41 -0400

    update

--- website-testing/next.config.js
+++ website-testing/next.config.js
@@ -2,3 +2,17 @@
 const nextConfig = {}
 
 module.exports = nextConfig
+
+// next.config.js
+module.exports = {
+    webpack: (config, { isServer }) => {
+      if (!isServer) {
+        config.node = {
+          fs: 'empty' // Allow loading markdown files as modules
+        };
+      }
+  
+      return config;
+    }
+  };
+  
